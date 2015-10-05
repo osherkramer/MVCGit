c306912cf3619d177f5c03b9979cb70e0af3255a
Osher Kramer <osherkramer@gmail.com>
2015-10-05 16:17:45 +0300

    Update propertiesGUI

--- MVP/src/propertiesGUI/Window.java
+++ MVP/src/propertiesGUI/Window.java
@@ -46,64 +46,64 @@ public class Window extends BasicWindow {
 		
 		Label mazeXSize = new Label(shell,SWT.NULL);
 		mazeXSize.setText("Maze  X:");
-		mazeXSize.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		mazeXSize.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 		
 		Text mazeXText = new Text(shell, SWT.SINGLE | SWT.BORDER);
-		mazeXText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		mazeXText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		    
 		Label mazeYSize = new Label(shell,SWT.NULL);
 		mazeYSize.setText("Maze  Y:");
-		mazeYSize.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		mazeYSize.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 			
 		Text mazeYText = new Text(shell, SWT.SINGLE | SWT.BORDER);
-		mazeYText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		mazeYText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
     
 			 
 		Label mazeZSize = new Label(shell,SWT.NULL);
 		mazeZSize.setText("Maze  Z:");
-		mazeZSize.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		mazeZSize.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 				
 		Text mazeZText = new Text(shell, SWT.SINGLE | SWT.BORDER);
-		mazeZText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		mazeZText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label numOfThreadsLabel = new Label(shell,SWT.NULL);
 		numOfThreadsLabel.setText("Number of Threads:");
-		numOfThreadsLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		numOfThreadsLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 				
 		Text numOfThreadsText = new Text(shell, SWT.SINGLE | SWT.BORDER);
-		numOfThreadsText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		numOfThreadsText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Label generateAlgo = new Label(shell,SWT.NULL);
 		generateAlgo.setText("Maze generator:");
-		generateAlgo.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		generateAlgo.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 		
 		Combo generateAlgoButton = new Combo(shell, SWT.NONE);
 		generateAlgoButton.setItems(new String[]{"My Maze generator","Simple Maze generator"});
-		generateAlgoButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		generateAlgoButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
 		
 		Label solveAlgo = new Label(shell,SWT.NULL);
 		solveAlgo.setText("Solve algorithem:");
-		solveAlgo.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		solveAlgo.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 		
 		Combo solveAlgoButton = new Combo(shell, SWT.NONE);
 		solveAlgoButton.setItems(new String[]{"BFS","A* Manhattan Distance","A* Air Distance"});
-		solveAlgoButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		solveAlgoButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
 		
 		Label uiLabel = new Label(shell,SWT.NULL);
 		uiLabel.setText("User Interface:");
-		uiLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
+		uiLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1));
 		
 		Combo uiButton = new Combo(shell, SWT.NONE);
-		uiButton.setItems(new String[]{"CLI","GUI"});
-		uiButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		uiButton.setItems(new String[]{"GUI","CLI"});
+		uiButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
 		
 		Button saveButton=new Button(shell, SWT.PUSH);
 		saveButton.setText("Save");
-		saveButton.setLayoutData(new GridData(SWT.FILL, SWT.None, false, false, 1, 1));
+		saveButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Button cancelButton=new Button(shell, SWT.PUSH);
 		cancelButton.setText("Exit");
-		cancelButton.setLayoutData(new GridData(SWT.FILL, SWT.None, false, false, 1, 1));
+		cancelButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		
 		Listener listener = new Listener() {
 			
